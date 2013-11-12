f0d1bdb6827e90e71c62660630b4a57cd9275118
MartinToma <martin.toma.work@gmail.com>
2013-11-12 21:28:12 +0100

    Fish, git, vim, uxterm files addedd

---
+++ config.fish
@@ -0,0 +1,39 @@
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  echo $branch
+end
+
+function fish_prompt
+   set_color yellow
+   printf '%s' (whoami)
+   set_color normal
+   printf ' at '
+
+   set_color magenta
+   printf '%s' (hostname|cut -d . -f 1)
+   set_color normal
+   printf ' in '
+
+   set_color $fish_color_cwd
+   printf '%s' (prompt_pwd)
+   set_color normal
+   
+   if test -d .git
+     printf ' on '
+     set_color yellow
+     printf '%s' (parse_git_branch)
+   end
+   set_color normal
+
+   echo -n ' '
+   echo -n [
+   echo -n (date +%X)
+   echo -n ]
+
+   echo
+   set_color $fish_color_cwd
+   printf 'cmd '
+   set_color normal
+   printf '-> '
+end
---
+++ vimrc
@@ -0,0 +1,24 @@
+" Author : Martin Toma
+" Date created : Tue Nov 12 14:14:56 CET 2013
+" vimrc config file for RAILS development
+"
+" Basic settings
+
+set nocompatible
+filetype plugin on
+set number
+syntax on
+
+" Disabling arrow keys
+map <up> <nop>
+map <down> <nop>
+map <left> <nop>
+map <right> <nop>
+
+" Intedation settings
+set autoindent
+set expandtab
+set shiftwidth=2
+set softtabstop=2
+
+" ...
