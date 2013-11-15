480f4e5adec0c4e5b20e3287ee9f409c5adf39d3
Martin Toma <mtoma@dvsk.sk>
2013-11-15 13:16:08 +0100

    Added install script and vim256 color run alternative

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ======= DotFiles installator ======
+# @author Martin Toma
+# @version 1.0
+# @date Fri Nov 15 13:13:22 CET 2013
+
+# ======= Necessary variables ======
+
+cpath=`pwd`       # config files path
+
+# ======= Installing all dotfiles ======
+
+cd ~
+
