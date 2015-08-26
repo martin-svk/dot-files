2e20a8ce8a9a2ab77ae730833b8e3a3fc93ad506
Martin Toma <martin.toma.svk@gmail.com>
2015-08-26 22:06:46 +0200

    Update devilspie config.

--- other/devilspie2/settings.lua
+++ other/devilspie2/settings.lua
@@ -7,28 +7,30 @@ debug_print("Application name: " .. get_application_name())
 -- Chromium
 if (string.find(get_application_name(), "Chromium") or get_application_name()=="Firefox") then
    set_window_workspace(1);
+   maximize();
 -- XTerm
 elseif (get_application_name()=="xterm") then
    set_window_workspace(2);
-   maximize();
--- DevDocs
-elseif (get_application_name()=="Untitled" or string.find(get_application_name(), "DevDocs")) then
+-- Android Studio
+elseif (get_application_name()=="Android Studio") then
    set_window_workspace(3);
    maximize();
--- Geary
+-- SmartGit
+elseif (get_application_name()=="SmartGit") then
+   set_window_workspace(4);
+   maximize();
+-- Music
+elseif (get_application_name()=="Spotify Free - Linux Preview" or get_application_name()=="VLC media player") then
+   set_window_workspace(5);
+   maximize();
+-- Mail
 elseif (get_application_name()=="Geary") then
    set_window_workspace(6);
    maximize();
 -- HipChat
 elseif (get_application_name()=="HipChat") then
    set_window_workspace(7);
-   maximize();
--- Spotify
-elseif (get_application_name()=="Spotify Free - Linux Preview") then
-   set_window_workspace(8);
-   maximize();
 -- Steam
 elseif (get_application_name()=="Steam") then
-   set_window_workspace(9);
-   maximize();
+   set_window_workspace(8);
 end
