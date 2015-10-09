b1d601b150b75b475e8dd53fe0c7a4bb619d5a54
Martin Toma <martin.toma.svk@gmail.com>
2015-10-09 15:56:38 +0200

    Set window sizes via devilspie2.

--- other/devilspie2/settings.lua
+++ other/devilspie2/settings.lua
@@ -10,12 +10,14 @@ if (string.find(get_application_name(), "Google Chrome")) then
 -- XTerm
 elseif (get_application_name()=="xterm") then
    set_window_workspace(2);
+   set_window_size(1600, 900);
 -- Android Studio
 elseif (get_application_name()=="Android Studio") then
    set_window_workspace(3);
 -- Atom
 elseif (get_application_name()=="Atom") then
    set_window_workspace(3);
+   set_window_size(1600, 900);
 -- SmartGit
 elseif (get_application_name()=="SmartGit") then
    set_window_workspace(4);
@@ -23,6 +25,7 @@ elseif (get_application_name()=="SmartGit") then
 -- Music
 elseif (get_application_name()=="Spotify Free - Linux Preview" or get_application_name()=="VLC media player") then
    set_window_workspace(5);
+   set_window_size(1600, 900);
 -- Mail
 elseif (get_application_name()=="Geary") then
    set_window_workspace(6);
