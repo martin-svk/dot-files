8ce4b266e62b312e680f9e58be2315e36cda37c5
Martin Toma <martin.toma.svk@gmail.com>
2015-10-11 11:32:58 +0200

    Make xterm and atom windows a little bit bigger.

--- other/devilspie2/settings.lua
+++ other/devilspie2/settings.lua
@@ -10,14 +10,14 @@ if (string.find(get_application_name(), "Google Chrome")) then
 -- XTerm
 elseif (get_application_name()=="xterm") then
    set_window_workspace(2);
-   set_window_size(1600, 900);
+   set_window_size(1600, 950);
 -- Android Studio
 elseif (get_application_name()=="Android Studio") then
    set_window_workspace(3);
 -- Atom
 elseif (get_application_name()=="Atom") then
    set_window_workspace(3);
-   set_window_size(1600, 900);
+   set_window_size(1600, 950);
 -- SmartGit
 elseif (get_application_name()=="SmartGit") then
    set_window_workspace(4);
