a3264b4e41b43cc624860d8c72c175d2d88ac662
Martin Toma <martin.toma.svk@gmail.com>
2015-11-02 09:49:26 +0100

    Maximize xterm window.

--- other/devilspie2/settings.lua
+++ other/devilspie2/settings.lua
@@ -10,7 +10,7 @@ if (string.find(get_application_name(), "Google Chrome")) then
 -- XTerm
 elseif (get_application_name()=="xterm") then
    set_window_workspace(2);
-   set_window_size(1600, 950);
+   maximize();
 -- Android Studio
 elseif (get_application_name()=="Android Studio") then
    set_window_workspace(3);
